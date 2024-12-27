8c8f6466d18a5089d3ab31ce3ae0afc633330187
chatla sarika <sarikachatla616@gmail.com>
2024-12-27 15:11:27 +0530

    kafkadriver

---
+++ cab-book-user/src/main/java/com/code/constant/AppConstant.java
@@ -0,0 +1,7 @@
+package com.code.constant;
+
+public class AppConstant {
+
+	public static final String CAB_LOCATION = "cab-location";
+
+}
---
+++ cab-book-user/src/main/java/com/code/service/CabLocationService.java
@@ -0,0 +1,21 @@
+package com.code.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import com.code.constant.AppConstant;
+
+@Service
+public class CabLocationService {
+	
+	
+	@Autowired
+	private KafkaTemplate<String, Object> kafkaTemplate;
+	
+	public boolean updateLocation(String location) {
+		kafkaTemplate.send(AppConstant.CAB_LOCATION,location);
+		return true;
+	}
+
+}
